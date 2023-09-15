19aa9dc46969044408569cfcccf17c20c5733350
Habib <habibullah.2004@yahoo.com>
2023-09-15 16:34:48 +0430

    Optimized

--- sickseries/settings.py
+++ sickseries/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django_cleanup.apps.CleanupConfig',
     'django.contrib.staticfiles',
     'store',
     'youtube',
